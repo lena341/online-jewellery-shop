5e8d0a5b16ad5f1b36edf129a9eaf4d965100277
Elena K <71977337+Lena341@users.noreply.github.com>
2023-04-20 21:01:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ function topFunction()
     document.body.scrollTop = 0; //For Safari
     document.documentElement.scrollTop = 0; //For Chrome, Firefox, IE and Opera
 }
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+});
